d2665f5cb9fde27ff97ff5960bc9612bedf874c6
Achilles-3v <sh17vv@gmail.com>
2024-04-25 23:32:26 +0500

    added new products forEach

--- manager-app/src/main/java/com/leater/manager/repository/InMemoryProductRepository.java
+++ manager-app/src/main/java/com/leater/manager/repository/InMemoryProductRepository.java
@@ -3,15 +3,21 @@ package com.leater.manager.repository;
 import com.leater.manager.entity.Product;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Repository
 public class InMemoryProductRepository implements ProductRepository {
 
     private final List<Product> products = Collections.synchronizedList(new LinkedList<>());
+
+    public InMemoryProductRepository() {
+        IntStream.range(1, 4)
+                .forEach(i -> this.products.add(new Product(i, "Изделие № %d".formatted(i),
+                        "Описание изделия № %d".formatted(i))));
+    }
 
     @Override
     public List<Product> fineAll() {
