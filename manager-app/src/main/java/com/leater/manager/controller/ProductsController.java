8f37721e80af6e200a37f0814e03a28062c7a643
Achilles-3v <sh17vv@gmail.com>
2024-04-29 15:19:49 +0500

    added validation

--- manager-app/src/main/java/com/leater/manager/controller/ProductsController.java
+++ manager-app/src/main/java/com/leater/manager/controller/ProductsController.java
@@ -3,9 +3,12 @@ package com.leater.manager.controller;
 import com.leater.manager.controller.payload.NewProductPayload;
 import com.leater.manager.entity.Product;
 import com.leater.manager.service.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -27,8 +30,18 @@ public class ProductsController {
     }
 
     @PostMapping("create")
-    public String createProduct(NewProductPayload payload) {
-        Product product = this.productService.createProduct(payload.title(), payload.details());
-        return "redirect:/catalogue/products/%d".formatted(product.getId());
+    public String createProduct(@Valid NewProductPayload payload,
+                                BindingResult bindingResult,
+                                Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("payload", payload);
+            model.addAttribute("errors", bindingResult.getAllErrors().stream()
+                    .map(ObjectError::getDefaultMessage)
+                    .toList());
+            return "catalogue/products/new_product";
+        } else {
+            Product product = this.productService.createProduct(payload.title(), payload.details());
+            return "redirect:/catalogue/products/%d".formatted(product.getId());
+        }
     }
 }
